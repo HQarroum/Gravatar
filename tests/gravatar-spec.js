b24b97b457f0f5a9f98397d7aaac6286a9a16075
HQarroum <hqm.post@gmail.com>
2015-12-12 11:02:18 -0800

    Using my own email address to conduct the test

--- tests/gravatar-spec.js
+++ tests/gravatar-spec.js
@@ -13,23 +13,23 @@ describe('The gravatar module', function () {
      * Gravatar URL creation.
      */
     it('should return a valid Gravatar URL given an e-mail address', function () {
-        var url = 'https://secure.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?default=404&rating=G&size=80';
-        gravatar.get.url('beau@dentedreality.com.au').should.be.eql(url);
+        var url = 'https://secure.gravatar.com/avatar/c38f77cb4c86487a5a551aa772c71938?default=404&rating=G&size=80';
+        gravatar.get.url('hqm.post@gmail.com').should.be.eql(url);
     });
 
     /**
      * Customizing Gravatar URL.
      */
     it('should return a valid Gravatar URL given an e-mail address and user options', function () {
-        var url = 'https://secure.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?default=identicon&rating=pg&size=200';
-        gravatar.get.url('beau@dentedreality.com.au', { defaultIcon: 'identicon', rating: 'pg', size: 200 }).should.be.eql(url);
+        var url = 'https://secure.gravatar.com/avatar/c38f77cb4c86487a5a551aa772c71938?default=identicon&rating=pg&size=200';
+        gravatar.get.url('hqm.post@gmail.com', { defaultIcon: 'identicon', rating: 'pg', size: 200 }).should.be.eql(url);
     });
 
     /**
      * Profiles retrieval test.
      */
     it('should be able to retrieve a list of profiles associated with an e-mail address', function (done) {
-        gravatar.get.profiles('beau@dentedreality.com.au').then(function (result) {
+        gravatar.get.profiles('hqm.post@gmail.com').then(function (result) {
             Array.isArray(result).should.be.eql(true);
             should.exist(result[0]);
             done();
@@ -42,7 +42,7 @@ describe('The gravatar module', function () {
      * Avatar resolution test.
      */
     it('should be able to resolve an avatar associated with an e-mail address', function (done) {
-        gravatar.resolve('beau@dentedreality.com.au').then(function (url) {
+        gravatar.resolve('hqm.post@gmail.com').then(function (url) {
             var urlRegexp = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
             url.should.match(urlRegexp);
             done();
